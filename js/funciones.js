653e6737b8c2e6fc0837e27ad723ad8f599cf433
jlochoau <jorgeluis812@hotmail.com>
2019-04-11 23:09:50 -0500

    11-04-2019
    Se finaliza el ultimo modulo de actividades

--- js/funciones.js
+++ js/funciones.js
@@ -14,14 +14,3 @@
 //   document.getElementById("ganancia").value = msn_ganancia + ganancia;
 // }
 
-// function calcular_res(){
-    
-//     var a = document.f1.b1.value;
-//     var b = document.f1.b2.value;
-//     var x = document.f1.b3.value;
-//     var r = document.f1.c.value;
-//     r1= a.toString()+b.toString();
-//     r=parseFloat(r1)*parseFloat(x);
-//    document.f1.c.value=r;
-
-// }
